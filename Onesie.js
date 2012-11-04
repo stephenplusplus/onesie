a68d33661924d798c1626111f42785132979651b
stephen <=>
2012-11-04 14:08:36 -0500

    spaces!!

--- Onesie.js
+++ Onesie.js
@@ -1,75 +1,75 @@
 var Onesie = (function(){
-	// Onesie.
-	var ActiveOnesie;
-	function Onesie (obj) {
-		ActiveOnesie = this;
-		this.fb = {
-			id: obj.fb_id,
-			fb_api_key:  obj.fb_api_key
-		};
-	};
-	Onesie.prototype.share = function (obj) {
-		var service = obj.service || 'fb';
+    // Onesie.
+    var ActiveOnesie;
+    function Onesie (obj) {
+        ActiveOnesie = this;
+        this.fb = {
+            id: obj.fb_id,
+            fb_api_key:  obj.fb_api_key
+        };
+    };
+    Onesie.prototype.share = function (obj) {
+        var service = obj.service || 'fb';
 
-		Services[service](this);
-	};
-	Onesie.prototype.Photo = function (obj) {
-		return new Photo(obj);
-	};
-	Onesie.prototype.Event = function (obj) {
-		return new Event(obj);
-	};
+        Services[service](this);
+    };
+    Onesie.prototype.Photo = function (obj) {
+        return new Photo(obj);
+    };
+    Onesie.prototype.Event = function (obj) {
+        return new Event(obj);
+    };
 
-	// Social Services.
-	var Services = (function() {
-		var connect = {};
-		connect.fb = function () {
-			var fbInstance = FB;
-			var fb_id = ActiveOnesie.fb.fb_id;
-			var fb_api_key = ActiveOnesie.fb.fb_api_key;
+    // Social Services.
+    var Services = (function() {
+        var connect = {};
+        connect.fb = function () {
+            var fbInstance = FB;
+            var fb_id = ActiveOnesie.fb.fb_id;
+            var fb_api_key = ActiveOnesie.fb.fb_api_key;
 
-			return fbInstance;
-		};
+            return fbInstance;
+        };
 
-		var Services = {};
-		Services.fb = function (thing) {
-			var FB = FB || connect.fb();
+        var Services = {};
+        Services.fb = function (thing) {
+            var FB = FB || connect.fb();
 
-			FB.share(thing); // <-- FB's api.
-		};
+            FB.share(thing); // <-- FB's api.
+        };
 
-		return Services;
-	})();
+        return Services;
+    })();
 
-	// Photo Helper.
-	var Photos = {};
-	function Photo (obj) {
-		Photos[obj.src] = this;
-		this.src = obj.src;
-	};
-	Photo.prototype.share = Onesie.prototype.share.bind(this);
-	Photo.prototype.getWidth = function () {
-		return 333;
-	};
-	Photo.prototype.getHeight = function () {
-		return 111;
-	};
-	Photo.prototype.getDimensions = function () {
-		var width = this.getWidth(),
-			height = this.getHeight();
-		return { width: width, height: height };
-	};
-	Photo.prototype.getFileSize = function () {
-		return this;
-	};
+    // Photo Helper.
+    var Photos = {};
+    function Photo (obj) {
+        Photos[obj.src] = this;
+        this.src = obj.src;
+    };
+    Photo.prototype.share = Onesie.prototype.share.bind(this);
+    Photo.prototype.getWidth = function () {
+        return 333;
+    };
+    Photo.prototype.getHeight = function () {
+        return 111;
+    };
+    Photo.prototype.getDimensions = function () {
+        var width = this.getWidth(),
+            height = this.getHeight();
+        return { width: width, height: height };
+    };
+    Photo.prototype.getFileSize = function () {
+        return this;
+    };
 
-	return Onesie;
+    return Onesie;
 })();
 
 /* instantiate our Onesie. */
 var helper = new Onesie({
-	fb_id: 'blahblah',
-	fb_api_key: 'hblahb'
+    fb_id: 'blahblah',
+    fb_api_key: 'hblahb'
 });
 
 /* let's create a new Photo helper to get additional functionality from just an image path. */
